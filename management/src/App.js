878e85b34202f0977f9c090fa871fe824d09ec29
Hongjun <denny6389147@gmail.com>
2020-06-02 21:32:25 +0900

    Applied material UI

--- management/src/App.js
+++ management/src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles'
 import './App.css';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
     {'id': 1,
      'profilePic': 'https://placeimg.com/64/64/1',
-     'name': 'Hongjun Kim',
+     'name': 'Sam',
      'major': 'Computer Science',
      'studentId': '260728949',
      'gender': 'Male'},
     {'id': 2,
      'profilePic': 'https://placeimg.com/64/64/2',
-     'name': 'Karsa',
+     'name': 'Tom',
      'major': 'Progamer',
      'studentId': '2017492093',
      'gender': 'Male'}
@@ -19,25 +37,32 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                profilePic={c.profilePic}
-                name={c.name}
-                major={c.major}
-                studentId={c.studentId}
-                gender={c.gender}
-              />
-            )
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>No.</TableCell>
+              <TableCell>Profile Pic</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Major</TableCell>
+              <TableCell>Student Id</TableCell>
+              <TableCell>Gender</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {customers.map(c => {
+                return (
+                  <Customer key={c.id} id={c.id} profilePic={c.profilePic} name={c.name}
+                    major={c.major} studentId={c.studentId} gender={c.gender}/>
+                )})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -1,44 +1,19 @@
 import React, { Component } from 'react';
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 class Customer extends Component {
   render() {
     return (
-      <div>
-        <CustomerProfile
-          id={this.props.id}
-          profilePic={this.props.profilePic}
-          name={this.props.name}/>
-        <CustomerInfo
-          major={this.props.major}
-          studentId={this.props.studentId}
-          gender={this.props.gender}/>
-      </div>
+        <TableRow>
+          <TableCell>{this.props.id}</TableCell>
+          <TableCell><img src={this.props.profilePic} alt="profile"/></TableCell>
+          <TableCell>{this.props.name}</TableCell>
+          <TableCell>{this.props.major}</TableCell>
+          <TableCell>{this.props.studentId}</TableCell>
+          <TableCell>{this.props.gender}</TableCell>
+        </TableRow>
     );
-  }
-}
-
-class CustomerInfo extends Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.major}</p>
-        <p>{this.props.studentId}</p>
-        <p>{this.props.gender}</p>
-      </div>
-    )
-  }
-}
-
-class CustomerProfile extends Component {
-  render() {
-    return (
-      <div>
-        <img
-          src={this.props.profilePic}
-          alt="profile"/ >
-        <h2>{this.props.name}</h2>
-      </div>
-    )
   }
 }
 
